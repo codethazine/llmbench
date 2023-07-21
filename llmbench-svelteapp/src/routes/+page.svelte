1bfa971955c7524615b43dcf10029afb903f9dd4
codethazine <dario.finocchiaro01@gmail.com>
2023-07-22 00:18:37 +0200

    Add footer and header

--- llmbench-svelteapp/src/routes/+page.svelte
+++ llmbench-svelteapp/src/routes/+page.svelte
@@ -1,11 +1,34 @@
 
-<div class="navbar bg-base-300">
+<header class="navbar bg-base-300">
     <div class="flex-1">
-        <a class="btn btn-ghost normal-case text-xl">LLMBench</a>
+        <a href="/" class="btn btn-ghost normal-case text-xl">LLMBench</a>
     </div>
     <div class="flex-none">
         <button class="btn btn-square btn-ghost">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
         </button>
     </div>
-</div>
+</header>
+
+<footer class="footer p-10 bg-neutral text-neutral-content">
+    <div>
+        <span class="footer-title">Services</span> 
+        <a href="/" class="link link-hover">Branding</a>
+        <a href="/" class="link link-hover">Design</a>
+        <a href="/" class="link link-hover">Marketing</a>
+        <a href="/" class="link link-hover">Advertisement</a>
+    </div> 
+    <div>
+        <span class="footer-title">Company</span> 
+        <a href="/" class="link link-hover">About us</a>
+        <a href="/" class="link link-hover">Contact</a>
+        <a href="/" class="link link-hover">Jobs</a>
+        <a href="/" class="link link-hover">Press kit</a>
+    </div> 
+    <div>
+        <span class="footer-title">Legal</span> 
+        <a href="/" class="link link-hover">Terms of use</a>
+        <a href="/" class="link link-hover">Privacy policy</a>
+        <a href="/" class="link link-hover">Cookie policy</a>
+    </div>
+</footer>
