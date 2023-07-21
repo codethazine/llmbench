5535395b772ff2e5c59f0b503b29d5860b3b0ecb
codethazine <dario.finocchiaro01@gmail.com>
2023-07-21 23:29:23 +0200

    Add Smelte

--- llmbench-svelteapp/rollup.config.js
+++ llmbench-svelteapp/rollup.config.js
@@ -4,6 +4,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import terser from '@rollup/plugin-terser';
 import resolve from '@rollup/plugin-node-resolve';
 import livereload from 'rollup-plugin-livereload';
+import smelte from "smelte/rollup-plugin-smelte.js";
 import css from 'rollup-plugin-css-only';
 
 const production = !process.env.ROLLUP_WATCH;
@@ -44,6 +45,26 @@ export default {
 				dev: !production
 			}
 		}),
+		smelte({ 
+			purge: production,
+			output: "public/global.css", // it defaults to static/global.css which is probably what you expect in Sapper 
+			postcss: [], // Your PostCSS plugins
+			whitelist: [], // Array of classnames whitelisted from purging
+			whitelistPatterns: [], // Same as above, but list of regexes
+			tailwind: { 
+			  colors: { 
+				primary: "#b027b0",
+				secondary: "#009688",
+				error: "#f44336",
+				success: "#4caf50",
+				alert: "#ff9800",
+				blue: "#2196f3",
+				dark: "#212121" 
+			  }, // Object of colors to generate a palette from, and then all the utility classes
+			  darkMode: true, 
+			}, 
+			// Any other props will be applied on top of default Smelte tailwind.config.js
+		}),		
 		// we'll extract any component CSS out into
 		// a separate file - better for performance
 		css({ output: 'bundle.css' }),
