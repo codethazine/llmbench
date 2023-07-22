223e3d44aaf588951dc3ab07fb5766ba87f94ad9
codethazine <dario.finocchiaro01@gmail.com>
2023-07-22 18:51:48 +0200

    Fix dates handling

--- llmbench-vueapp/src/components/single_charts/MathProblemChart.vue
+++ llmbench-vueapp/src/components/single_charts/MathProblemChart.vue
@@ -5,24 +5,30 @@
   </template>
   
   <script>
-  import { onMounted, ref, toRefs } from 'vue';
+  import { onMounted, ref, toRefs, watch } from 'vue';
   import { Chart, BarController, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
-  
+    
   Chart.register(BarController, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
-
+  
   function getMonthName(dateString) {
     const date = new Date(dateString);
     return date.toLocaleString('default', { month: 'long' });
   }
-    
+
+  
+  
   export default {
     name: 'MathProblemChart',
     props: ['fromMonth', 'toMonth', 'modelData', 'modelId'],
     setup(props) {
       const { fromMonth, toMonth } = toRefs(props);
       const chart = ref(null);
-
-      onMounted(() => {
+  
+      const createChart = () => {
+        if (chart.value) {
+          chart.value.destroy(); // Destroy the old chart before creating a new one
+        }
+        
         chart.value = new Chart(document.getElementById('mathProblemChart'), {
           type: 'bar',
           data: {
@@ -49,10 +55,14 @@
             }
           }
         });
-      });
+      };
+  
+      onMounted(createChart);
+  
+      watch([fromMonth, toMonth], createChart); // Recreate the chart when fromMonth or toMonth changes
   
       return { chart };
     }
   }
-  </script>
+  </script>  
   
